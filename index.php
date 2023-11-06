3b649cf795bee1cdcca47a6694b5e88bb875f37f
unknown <sebastianfermaxi@gmail.com>
2023-11-06 01:49:37 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="http://localhost/API1-bien/backend/css/bootstrap.min.css">
+    <link rel="stylesheet" href="http://localhost/API1-bien/backend/css/estilos.css">
+    <title>Minitecnologia</title>
+    <link rel="shortcut icon" href="http://localhost/API1-bien/frontend/assets/img/fav.ico" type="image/jpeg">
+</head>
+<body>
+    <?php
+    
+    include 'frontend/views/inicio.html';
+    include 'frontend/views/categorias.html';
+    include 'frontend/views/agregar-categoria.html'    
+    ?>
+
+</body>
+</html>
